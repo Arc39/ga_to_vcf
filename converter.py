cc0afa71e927bc7809d2a9fadecb6ffe4bfa6862
Arc39 <kaspencer@csumb.edu>
2017-01-23 16:25:01 -0800

    Header function

--- converter.py
+++ converter.py
@@ -2,13 +2,13 @@ class converter(AbstractConverter):
 
 	def writeHeader(self,opFile):
 		variantSet = self._container
-		opFile.write("##fileformat=VCFv4.1")
-		opFile.write("##datasetid={datasetid}".format(datasetid=variantSet.datasetId))
-        opFile.write("##variantSetId={id}".format(id=variantSet.id))
+        print("##fileformat=VCFv4.1")
+        print("##datasetid={datasetid}".format(datasetid=variantSet.dataset_id))
+        print("##variantSetId={id}".format(id=variantSet.id))
 
         for metadata in variantSet.metadata:
-        	opFile.write("##INFO=<ID={id}".format(id=metadata.id),"Number={number}".format(number=metadata.number),"Type={type}".format(type=metadata.type),"Description={description}".format(description=metadata.description),"Info={info}".format(info=metadata.info),"value={value}".format(value=metadata.value),"key={key}>".format(key=metadata.key))
-		opFile.write("#CHROM POS ID REF ALT QUAL FILTER INFO")
+            print("##INFO=<ID={key}".format(key=metadata.key),"Number={number}".format(number=metadata.number),"Type={type}".format(type=metadata.type),"Description={description}".format(description=metadata.description),"Info={info}".format(info=metadata.info),"value={value}".format(value=metadata.value))
+        print("#CHROM POS ID REF ALT QUAL FILTER INFO")
 
 	
 	def writeVariants(self,opFile):
@@ -18,7 +18,7 @@ class converter(AbstractConverter):
 			opfile.write(variant)
 
 	def writeVCF(self):
-		opFile = open('self._outputFile', 'w')
+		opFile = open(self._outputFile, 'w')
 		self.writeHeader(opfile)
 		self.writeVariants(opfile)
 		opfile.close()

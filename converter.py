d4b4a06a3416b0113e84fe3ac6380af7dc5ca25a
Arc39 <kaspencer@csumb.edu>
2017-01-23 17:09:26 -0800

    tab/space issue

--- converter.py
+++ converter.py
@@ -1,23 +1,23 @@
 class converter(AbstractConverter):
 
-	def writeHeader(self):
-		variantSet = self._container
-		outputFile = self._outputFile
+    def writeHeader(self):
+        variantSet = self._container
+        outputFile = self._outputFile
         outputFile.write("##fileformat=VCFv4.1")
         outputFile.write("##datasetid={datasetid}".format(datasetid=variantSet.dataset_id))
         outputFile.write("##variantSetId={id}".format(id=variantSet.id))
 
         for metadata in variantSet.metadata:
-            outputFile.write("##INFO=<ID={key}".format(key=metadata.key),"Number={number}".format(number=metadata.number),"Type={type}".format(type=metadata.type),"Description={description}".format(description=metadata.description),"Info={info}".format(info=metadata.info),"value={value}".format(value=metadata.value))
+            outputFile.write("##INFO=<ID={key},Number={number},Type={type},Description={description},Info={info},value={value}".format(key=metadata.key,number=metadata.number,type=metadata.type,description=metadata.description,info=metadata.info,value=metadata.value))
         outputFile.write("#CHROM POS ID REF ALT QUAL FILTER INFO")
 
-	
-	def writeVariants(self):
-		varIter = self._objectIterator
+    
+    def writeVariants(self):
+        varIter = self._objectIterator
 
-		for variant in varIter:
-			print variant
+        for variant in varIter:
+            print variant
 
-	def writeVCF(self):
-		self.writeHeader()
-		self.writeVariants()
+    def writeVCF(self):
+        self.writeHeader()
+        self.writeVariants()

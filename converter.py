436036d0381e1b533b72aa9e45c2c3b9324d9817
Arc39 <kaspencer@csumb.edu>
2017-01-31 18:18:54 -0800

    Added > at end of line

--- converter.py
+++ converter.py
@@ -8,16 +8,9 @@ class converter(AbstractConverter):
         outputFile.write("##variantSetId={id}".format(id=variantSet.id))
 
         for metadata in variantSet.metadata:
-            outputFile.write("##INFO=<ID={key},Number={number},Type={type},Description={description},Info={info},value={value}".format(key=metadata.key,number=metadata.number,type=metadata.type,description=metadata.description,info=metadata.info,value=metadata.value))
+            outputFile.write("##INFO=<ID={key},Number={number},Type={type},Description={description},Info={info},value={value}>".format(key=metadata.key,number=metadata.number,type=metadata.type,description=metadata.description,info=metadata.info,value=metadata.value))
         outputFile.write("#CHROM POS ID REF ALT QUAL FILTER INFO")
 
     
     def writeVariants(self):
         varIter = self._objectIterator
-
-        for variant in varIter:
-            print variant
-
-    def writeVCF(self):
-        self.writeHeader()
-        self.writeVariants()
